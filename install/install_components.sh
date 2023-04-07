5998c37ba6a9e78af490eadb08b4c48cf3f7205f
alpeza <80060968+alpeza@users.noreply.github.com>
2023-04-07 12:50:27 +0200

    update

--- install/install_components.sh
+++ install/install_components.sh
@@ -13,3 +13,27 @@ cliInstall(){
 cliInstall
 
 # Core 
+cloneCore(){
+    FORCE_CLONE=0 #Si vale 1 elimina los repositorios descargados
+    # Descargar el archivo corerepos.txt en la carpeta /tmp
+    curl -o /tmp/corerepos.txt https://raw.githubusercontent.com/HomeCI/homeCI/main/install/corerepos.txt
+    # Cambiar al directorio $CORE_PATH
+    cd "$CORE_PATH"
+    # Leer el archivo corerepos.txt y clonar los repositorios con git clone
+    while read -r repo; do
+    # Extraer el nombre de la carpeta del repositorio de la URL
+    repo_name=$(basename "$repo" ".git")
+    # Comprobar si el repositorio ya existe
+    if [ -d "$repo_name" ]; then
+        # Si la variable $FORCE_CLONE es 1, eliminar y clonar de nuevo
+        if [ "$FORCE_CLONE" -eq 1 ]; then
+        rm -rf "$repo_name"
+        git clone -q "$repo"
+        else
+        echo "El repositorio $repo_name ya existe en $CORE_PATH"
+        fi
+    else
+        git clone -q "$repo"
+    fi
+    done < /tmp/corerepos.txt
+}
